12cf709ff5aac0d7da18850ed178caae2b1492b4
Matthew Linder <mattsasa@me.com>
2020-05-14 22:40:50 -0700

    init basic webpack and babel

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// JavaScript source code
+
+import moment from "moment"
+
+console.log("meow")
+console.log(moment().format())
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+};

2b52c09b1f8b1c763cfa7c635245c7cf742cf024
Matthew Linder <mattsasa@me.com>
2020-06-21 15:17:56 -0700

    updated website to use requestAnimationFrame

--- src/index.js
+++ src/index.js
@@ -2,18 +2,84 @@ import "./template.css"
 import { GameInstance as Game } from "./game/Game"
 import { n64GfxProcessorInstance as GFX } from "./graphics/n64GfxProcessor"
 
-window.frameSpeed = 33
+const send_display_list = (gfx_list) => {
+    start_render = performance.now()
+    GFX.run(gfx_list)
+}
 
-let start_render = 0
+let n_frames = 0
+const produce_one_frame = () => {
+
+    if (n_frames > 100000) { throw "Hit max frames" }
+    //console.log("new frame: " + n_frames)
+    n_frames++
+
+    GFX.start_frame()
+    Game.main_loop_one_iteration()
+
+    /// Audio TODO
+
+    GFX.end_frame()
+
+}
+
+const runGameWithMetrics = () => {
+
+    requestAnimationFrame(runGameWithMetrics)
+
+    const elapsed = performance.now() - last_frame_start
+    if (elapsed > frameSpeed) {
+        const start_frame = performance.now()
+        last_frame_start = start_frame - (elapsed % frameSpeed)
+        produce_one_frame()
+        const finished_frame = performance.now()
+        webpage_update()
+        gameLogicFrameTimeBuffer.push(start_render - start_frame)
+        renderFrameTimeBuffer.push(finished_frame - start_render)
+        totalFrameTimeBuffer.push(finished_frame - start_frame)
+    }
+}
+
+const main_func = () => {
+
+    /// WebGL class and n64GfxProcessor class are initialized with their constructor when they are imported
+    Game.attachInterfaceToGfxProcessor(send_display_list)
+
+    runGameWithMetrics()
+
+}
+
+
+//////////////////// Some more website stuff
+
+window.addEventListener("load", function () {
+    var elements = document.getElementsByClassName("rainbowText")
+    for (let i = 0; i < elements.length; i++) {
+        generateRainbowText(elements[i])
+    }
+})
+
+const letterColors = ["blue", "red", "green", "yellow"]
+
+function generateRainbowText(element) {
+    var text = element.innerText
+    element.innerHTML = ""
+    for (let i = 0; i < text.length; i++) {
+        let charElem = document.createElement("span")
+        charElem.style.color = letterColors[i % 4]
+        charElem.innerHTML = text[i]
+        element.appendChild(charElem)
+    }
+}
 
 const createRingBuffer = (length) => {
-    let pointer = 0
+    let index = 0
     const buffer = []
 
     return {
         push: (item) => {
-            buffer[pointer] = item
-            pointer = (pointer + 1) % length
+            buffer[index] = item
+            index = (index + 1) % length
             return item
         },
         getAvg: () => {
@@ -26,10 +92,6 @@ const totalFrameTimeBuffer = createRingBuffer(10)
 const renderFrameTimeBuffer = createRingBuffer(10)
 const gameLogicFrameTimeBuffer = createRingBuffer(10)
 
-const timeout = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
-
 const setStatsUpdate = setInterval(() => {
     const totalFrameTimeAvg = totalFrameTimeBuffer.getAvg().toFixed(2)
     const renderFrameTimeAvg = renderFrameTimeBuffer.getAvg().toFixed(2)
@@ -39,59 +101,24 @@ const setStatsUpdate = setInterval(() => {
     document.getElementById("timing-total").innerHTML = `${totalFrameTimeAvg}ms`
     document.getElementById("timing-game").innerHTML = `${gameLogicFrametimeAvg}ms`
     document.getElementById("timing-render").innerHTML = `${renderFrameTimeAvg}ms`
-
-}, 333)
+}, 500)
 
 const webpage_update = () => {
     document.getElementById("numTriangles").innerHTML = `Total Triangles this frame: ${window.totalTriangles}`
-    document.getElementById("fps").innerHTML = `${ parseInt(1000 / window.frameSpeed)  } fps`
 }
 
-let n_frames = 0
+document.addEventListener('change', (event) => {
+    frameSpeed = 1000 / event.target.value
+    document.getElementById("fps").innerHTML = `${parseInt(1000 / frameSpeed)} fps`
+})
+//////////////
 
-const send_display_list = (gfx_list) => {
-    start_render = performance.now()
-    GFX.run(gfx_list)
-}
 
-const produce_one_frame = () => {
+let frameSpeed = 33.3
 
-    if (n_frames > 100000) { throw "Hit max frames" }
-    //console.log("new frame: " + n_frames)
-    n_frames++
+let start_render = 0
+let last_frame_start = 0
 
-    GFX.start_frame()
-    Game.main_loop_one_iteration()
-
-    /// Audio
-
-    GFX.end_frame()
-
-}
-
-const main_func = async () => {
-
-    /// WebGL class and n64GfxProcessor class are initialized with their constructor when they are imported
-    Game.attachInterfaceToGfxProcessor(send_display_list)
-
-    while (1) {
-
-        const start = performance.now()
-
-        produce_one_frame()
-
-        /// Webpage
-        const totalFrameTime = performance.now() - start
-        gameLogicFrameTimeBuffer.push(start_render - start)
-        renderFrameTimeBuffer.push(performance.now() - start_render)
-        totalFrameTimeBuffer.push(performance.now() - start)
-        webpage_update()
-
-        await timeout(window.frameSpeed - totalFrameTime)
-    }
-    
-}
-
+/// Start
 console.log("Starting Application!")
 main_func()
-

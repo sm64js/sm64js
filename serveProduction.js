fe94d9a6f69c2085760ac30ba109c275817a10b8
Matthew Linder <mattsasa@me.com>
2020-06-28 14:46:11 -0500

    refactored rom extraction on server and refactored texture loader on client, added optional external rom param to load rom from another location

--- serveProduction.js
+++ serveProduction.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const fileUpload = require('express-fileupload')
-const server = require('http').Server(app)
+const http = require('http')
+const server = http.Server(app)
 const fs = require('fs')
 const { v4: uuidv4 } = require('uuid')
 const { promisify } = require('util')
@@ -25,10 +26,62 @@ const hexDumpExtract = (dir) => {
   })
 }
 
+const fileDownload = (file, url) => {
+    return new Promise((resolve, reject) => {
+        try {
+            http.get(url, (response) => {
+                const stream = response.pipe(file)
+                stream.on('error', () => { reject('Fail') })
+                stream.on('finish', () => { resolve() })
+            })
+        } catch {
+            console.log("HTTP GET Error")
+            fs.rmdirSync('extractTools/' + uid, { recursive: true })
+            reject('Fail')
+        }
+    })
+}
+
+const extractJsonFromRomFile = async (dir) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            await pythonExtract(dir)
+            await hexDumpExtract(dir)
+
+            const extractedData = {}
+            const assets = JSON.parse(fs.readFileSync('extractTools/assets.json'))
+            Object.keys(assets).forEach((assetname) => {
+                let filepath = assetname
+                if (filepath == '@comment') return
+                filepath = `extractTools/${dir}/${filepath}`
+                filepath = filepath.substring(0, filepath.length - 4) + ".js"
+                const filedata = fs.readFileSync(filepath, "utf8")
+                extractedData[assetname] = filedata
+            })
+            fs.rmdirSync('extractTools/' + dir, { recursive: true })
+            resolve(extractedData)
+        } catch {
+            console.log('Rom Extraction Fail')
+            fs.rmdirSync('extractTools/' + dir, { recursive: true })
+            resolve('Fail')
+        }
+    })
+}
+
 app.use(express.static(__dirname + '/dist'))
 app.use(fileUpload())
-server.listen(port, () => {
-	console.log('Serving Files')
+server.listen(port, () => { console.log('Serving Files') })
+
+app.get("/romTransfer", async (req, res) => {
+
+    const uid = uuidv4()
+    await mkdir('extractTools/' + uid)
+
+    const file = fs.createWriteStream('extractTools/' + uid + '/baserom.us.z64')
+    await fileDownload(file, 'http://' + req.query.romExternal)
+
+    res.send(await extractJsonFromRomFile(uid))
+
 })
 
 app.post("/romUpload", async (req, res) => {
@@ -42,26 +95,7 @@ app.post("/romUpload", async (req, res) => {
 			return res.send('Fail')
 		}
 
-        try {
-            await pythonExtract(uid)
-            await hexDumpExtract(uid)
-            const extractedData = {}
-            const assets = JSON.parse(fs.readFileSync('extractTools/assets.json'))
-            Object.keys(assets).forEach((assetname) => {
-                let filepath = assetname
-                if (filepath == '@comment') return
-                filepath = `extractTools/${uid}/${filepath}`
-                filepath = filepath.substring(0, filepath.length - 4) + ".js"
-                const filedata = fs.readFileSync(filepath, "utf8")
-                extractedData[assetname] = filedata
-            })
-            fs.rmdirSync('extractTools/' + uid, { recursive: true })
-            return res.send(extractedData)
-        } catch {
-            fs.rmdirSync('extractTools/' + uid, { recursive: true })
-            return res.send('Fail')
-        }
-
+        res.send(await extractJsonFromRomFile(uid))
 
 	})
 })

2ecf444a501a127daf568a7589d6c8a73fa3a201
Matthew Linder <mattsasa@me.com>
2020-06-28 11:44:28 -0500

    finished removing textured and importing them from ROM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import "./template.css"
-import { GameInstance as Game } from "./game/Game"
 import { n64GfxProcessorInstance as GFX } from "./graphics/n64GfxProcessor"
+import { intro_seg7_texture_070086A0, intro_seg7_texture_07007EA0, intro_seg7_texture_0700B4A0, intro_seg7_texture_0700C4A0 } from "./levels/intro/leveldata"
+import { title_texture_0A0001C0, title_texture_0A000E40, title_texture_0A001AC0, title_texture_0A002740 } from "./levels/intro/title_screen_bg"
+import {
+    gd_texture_mario_face_shine,
+    gd_texture_red_star_0,
+    gd_texture_red_star_1,
+    gd_texture_red_star_2,
+    gd_texture_red_star_3,
+    gd_texture_red_star_4,
+    gd_texture_red_star_5,
+    gd_texture_red_star_6,
+    gd_texture_red_star_7,
+    gd_texture_white_star_0,
+    gd_texture_white_star_1,
+    gd_texture_white_star_2,
+    gd_texture_white_star_3,
+    gd_texture_white_star_4,
+    gd_texture_white_star_5,
+    gd_texture_white_star_6,
+    gd_texture_white_star_7,
+    gd_texture_sparkle_0,
+    gd_texture_sparkle_1,
+    gd_texture_sparkle_2,
+    gd_texture_sparkle_3,
+    gd_texture_sparkle_4
+ } from "./goddard/GoddardRenderer"
+import { GameInstance as Game } from "./game/Game"
+
 
 const send_display_list = (gfx_list) => {
     start_render = performance.now()
@@ -65,8 +92,38 @@ $('#romUpload').submit(
             data: new FormData(e.target),
             processData: false,
             contentType: false,
-            success: (result) => {
-                console.log(result)
+            success: (extractedData) => {
+                intro_seg7_texture_070086A0.push( ...extractedData['levels/intro/1.rgba16.png'].split(',') )
+                intro_seg7_texture_07007EA0.push( ...extractedData['levels/intro/0.rgba16.png'].split(',') )
+                intro_seg7_texture_0700B4A0.push( ...extractedData['levels/intro/2_copyright.rgba16.png'].split(','))
+                intro_seg7_texture_0700C4A0.push( ...extractedData['levels/intro/3_tm.rgba16.png'].split(','))
+                gd_texture_mario_face_shine.push( ...extractedData['textures/intro_raw/mario_face_shine.ia8.png'].split(','))
+                gd_texture_red_star_0.push( ...extractedData['textures/intro_raw/red_star_0.rgba16.png'].split(','))
+                gd_texture_red_star_1.push( ...extractedData['textures/intro_raw/red_star_1.rgba16.png'].split(','))
+                gd_texture_red_star_2.push( ...extractedData['textures/intro_raw/red_star_2.rgba16.png'].split(','))
+                gd_texture_red_star_3.push( ...extractedData['textures/intro_raw/red_star_3.rgba16.png'].split(','))
+                gd_texture_red_star_4.push( ...extractedData['textures/intro_raw/red_star_4.rgba16.png'].split(','))
+                gd_texture_red_star_5.push( ...extractedData['textures/intro_raw/red_star_5.rgba16.png'].split(','))
+                gd_texture_red_star_6.push( ...extractedData['textures/intro_raw/red_star_6.rgba16.png'].split(','))
+                gd_texture_red_star_7.push( ...extractedData['textures/intro_raw/red_star_7.rgba16.png'].split(','))
+                gd_texture_white_star_0.push( ...extractedData['textures/intro_raw/white_star_0.rgba16.png'].split(','))
+                gd_texture_white_star_1.push( ...extractedData['textures/intro_raw/white_star_1.rgba16.png'].split(','))
+                gd_texture_white_star_2.push( ...extractedData['textures/intro_raw/white_star_2.rgba16.png'].split(','))
+                gd_texture_white_star_3.push( ...extractedData['textures/intro_raw/white_star_3.rgba16.png'].split(','))
+                gd_texture_white_star_4.push( ...extractedData['textures/intro_raw/white_star_4.rgba16.png'].split(','))
+                gd_texture_white_star_5.push( ...extractedData['textures/intro_raw/white_star_5.rgba16.png'].split(','))
+                gd_texture_white_star_6.push( ...extractedData['textures/intro_raw/white_star_6.rgba16.png'].split(','))
+                gd_texture_white_star_7.push( ...extractedData['textures/intro_raw/white_star_7.rgba16.png'].split(','))
+                gd_texture_sparkle_0.push( ...extractedData['textures/intro_raw/sparkle_0.rgba16.png'].split(','))
+                gd_texture_sparkle_1.push( ...extractedData['textures/intro_raw/sparkle_1.rgba16.png'].split(','))
+                gd_texture_sparkle_2.push( ...extractedData['textures/intro_raw/sparkle_2.rgba16.png'].split(','))
+                gd_texture_sparkle_3.push( ...extractedData['textures/intro_raw/sparkle_3.rgba16.png'].split(','))
+                gd_texture_sparkle_4.push( ...extractedData['textures/intro_raw/sparkle_4.rgba16.png'].split(','))
+                title_texture_0A0001C0.push( ...extractedData['textures/title_screen_bg/title_screen_bg.001C0.rgba16.png'].split(','))
+                title_texture_0A000E40.push( ...extractedData['textures/title_screen_bg/title_screen_bg.00E40.rgba16.png'].split(','))
+                title_texture_0A001AC0.push( ...extractedData['textures/title_screen_bg/title_screen_bg.01AC0.rgba16.png'].split(','))
+                title_texture_0A002740.push( ...extractedData['textures/title_screen_bg/title_screen_bg.02740.rgba16.png'].split(','))
+                document.getElementById("startbutton").disabled = false
             }
         })
     }
@@ -132,7 +189,7 @@ document.getElementById("slider").addEventListener('change', (event) => {
     document.getElementById("fps").innerHTML = `${event.target.value} fps`
 })
 
-document.getElementById("start").addEventListener('click', () => {
+document.getElementById("startbutton").addEventListener('click', () => {
     /// Start
     console.log("Starting Game!")
     main_func()

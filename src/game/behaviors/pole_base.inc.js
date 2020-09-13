f36927bcbbb9c41c04a4506aab48ea2886ec4a47
snuffysasa <mattsasa@me.com>
2020-09-12 18:29:54 -0700

    fixed glitch with jumping off trees slamming mario into ground, came from the updated push mario away function

--- src/game/behaviors/pole_base.inc.js
+++ src/game/behaviors/pole_base.inc.js
@@ -1,7 +1,7 @@
 import { ObjectListProcessorInstance as ObjectListProc } from "../ObjectListProcessor"
 import { oPosY, oTimer } from "../../include/object_constants"
 import { LevelUpdateInstance as LevelUpdate } from "../LevelUpdate"
-import { MARIO_PUNCHING } from "../Mario"
+import { MARIO_PUNCHING, ACT_GROUP_MASK, ACT_GROUP_AIRBORNE } from "../Mario"
 import { cur_obj_push_mario_away } from "../ObjectHelpers"
 
 export const bhv_pole_base_loop = () => {
@@ -9,7 +9,9 @@ export const bhv_pole_base_loop = () => {
     if (o.rawData[oPosY] - 10.0 < ObjectListProc.gMarioObject[0].rawData[oPosY] &&
         ObjectListProc.gMarioObject[0].rawData[oPosY] < o.rawData[oPosY] + o.hitboxHeight + 30.0) {
         if (o.rawData[oTimer] > 10) {
-            if (!(LevelUpdate.gMarioState[0].action & MARIO_PUNCHING)) {
+            if (!(LevelUpdate.gMarioState[0].action & MARIO_PUNCHING) &&
+                (LevelUpdate.gMarioState[0].action & ACT_GROUP_MASK) != ACT_GROUP_AIRBORNE
+            ) {
                 cur_obj_push_mario_away(70.0)
             }
         }

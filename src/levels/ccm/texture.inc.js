1a6c7a8cc5598f5037c001828005f15ff77df4aa
luphoria <luphoria@innocent.com>
2020-11-07 21:59:55 -0600

    Added models and textures for CCM

---
+++ src/levels/ccm/texture.inc.js
@@ -0,0 +1,40 @@
+export const ccm_seg7_texture_07000000 = []
+
+export const ccm_seg7_texture_07000800 = []
+
+export const ccm_seg7_texture_07000900 = []
+
+export const ccm_seg7_texture_07001100 = []
+
+export const ccm_seg7_texture_07001900 = []
+
+export const ccm_seg7_texture_07002100 = []
+
+export const ccm_seg7_texture_07002900 = []
+
+export const ccm_seg7_texture_07003100 = []
+
+export const ccm_seg7_texture_07003900 = []
+
+export const ccm_seg7_texture_07003B00 = []
+
+export const ccm_seg7_texture_07004300 = []
+
+export const ccm_seg7_texture_07004B00 = []
+
+/*
+import {
+	ccm_seg7_texture_07000000,
+	ccm_seg7_texture_07000800,
+	ccm_seg7_texture_07000900,
+	ccm_seg7_texture_07001100,
+	ccm_seg7_texture_07001900,
+	ccm_seg7_texture_07002100,
+	ccm_seg7_texture_07002900,
+	ccm_seg7_texture_07003100,
+	ccm_seg7_texture_07003900,
+	ccm_seg7_texture_07003B00,
+	ccm_seg7_texture_07004300,
+	ccm_seg7_texture_07004B00
+} from "../../../texture.inc"
+*/

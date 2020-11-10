16d3867ec246343497c6ce9cbab5c7ee525dc42c
sm64js <67404752+sm64js@users.noreply.github.com>
2020-11-09 19:59:12 -0800

    Update convertGeo.js

--- otherTools/convertGeo.js
+++ otherTools/convertGeo.js
@@ -4,10 +4,11 @@ const fs = require('fs')
 //You no longer need to touch snum or num, it checks if the files exist.
 
 // Configure these variables to get it to work
-var level = "ttm" // level name in sm64ex directory
+var level = "ccm" // level name in sm64ex directory
+var baseGeo = true // whether you're converting the base geo.inc or one within a model directory
 var snum = 1 // used as a counter variable (Keep 1!)
 var num = 32 // number of model.inc.js files there are
-var areaNum = 1 // target area number
+var areaNum = 2 // target area number
 var mainDir = __dirname + '/converted/' + level + '/areas/' + areaNum + '/' // directory to put models in
 
 //Not sure whether we need to skip commands.
@@ -76,108 +77,113 @@ function SearchAndApplyDLs(directory,reference,areaN) {
 
 // Preemptively make the main directory for output to avoid issues.
 MakeDirectory(mainDir);
-
-while (snum <= num) {
-
-var input = require('os').homedir() + '/sm64ex/levels/' + level + '/areas/' +areaNum+ '/' + snum + '/geo.inc.c' // directory of each model file
-var AreaDir = require('os').homedir() + '/sm64ex/levels/' + level + '/areas/' +areaNum+ '/' + snum + '/' // used for DL checks
-var AdditionalFiles = ""
-if (fs.existsSync(input)) {
-var inputStr = fs.readFileSync(input, 'utf8')
-var inputStr = inputStr.replace(/\r/g, "")
-var lines = inputStr.split("\n")
-var lines = lines.filter(line => (line.length != 0) && (line[0] != '/'))
-var outputStr = ""
-
-//Quick fix for skybox/camera loading.
-var LoadCamera = false
-var LoadSkybox = false
-
-//Quick fix for adding referenced files. Should work.
-
-//TODO AND IMPORTANT: Go through this and rewrite most of it so we can access each arg by itself.
-lines.forEach(lineParse => {
-	if (lineParse.trim().slice(0, 2) != "//") {
-		if (lineParse.trim().slice(0, 16) == 'const GeoLayout ') { //geolayout
-			const geoLayoutName = lineParse.trim().slice(16, lineParse.trim().indexOf('['))
-			outputStr += `export const ${geoLayoutName} = [\n`
-		} else if (lineParse.trim().slice(0, 2) == '};') { //geolayout end(?) (hack)
-			const geoLayoutName = lineParse.trim().slice(16, lineParse.trim().indexOf('['))
-			outputStr += `]\n`
-		} else if (lineParse.trim().slice(0, 18) == 'GEO_CULLING_RADIUS') { //cull radius
-			const cullRadius = lineParse.trim().slice(19, lineParse.trim().indexOf('),'))
-			outputStr += `{ command: Geo.node_culling_radius, args: [${cullRadius}] },\n`
-		} else if (lineParse.trim().slice(0, 13) == 'GEO_OPEN_NODE') { //open node
-			outputStr += `{ command: Geo.open_node },\n`
-		} else if (lineParse.trim().slice(0, 14) == 'GEO_CLOSE_NODE') { //close node
-			outputStr += `{ command: Geo.close_node },\n`
-		} else if (lineParse.trim().slice(0, 7) == 'GEO_END') { //end
-			outputStr += `{ command: Geo.node_end },\n`
-		} else if (lineParse.trim().slice(0, 14) == 'GEO_RENDER_OBJ') { //end
-			outputStr += `{ command: Geo.node_render_object_parent },\n`
-		} else if (lineParse.trim().slice(0, 16) == 'GEO_RENDER_RANGE') { //render range
-			const renderRange = lineParse.trim().slice(17, lineParse.trim().indexOf('),'))
-			outputStr += `{ command: Geo.node_render_range, args: [${renderRange}] },\n`
-		} else if (lineParse.trim().slice(0, 16) == 'GEO_DISPLAY_LIST') { //dl ----------- TODO : Make this search files in the same folder for the DL it's trying to render. 
-			const dList = lineParse.trim().slice(17, lineParse.trim().indexOf('),'))
-			const ArrayArgs = dList.split(', ')
-			AdditionalFiles += SearchAndApplyDLs(AreaDir,ArrayArgs[1],snum)
-			outputStr += `{ command: Geo.display_list, args: [Geo.${dList}] },\n`
-		} else if (lineParse.trim().slice(0, 7) == 'GEO_ASM') { //dl
-			const asm = lineParse.trim().slice(8, lineParse.trim().indexOf('),'))
-			outputStr += `{ command: Geo.node_generated, args: [${asm}]},\n`
-		} else if (lineParse.trim().slice(0, 11) == 'GEO_ZBUFFER') { //dl
-			const zb = lineParse.trim().slice(12, lineParse.trim().indexOf('),'))
-			outputStr += `{ command: Geo.node_master_list, args: [${zb}]},\n`
-		} else if (lineParse.trim().slice(0, 14) == 'GEO_NODE_ORTHO') { //dl
-			const ortho = lineParse.trim().slice(15, lineParse.trim().indexOf('),'))
-			outputStr += `{ command: Geo.node_ortho, args: [${ortho}]},\n`
-		} else if (lineParse.trim().slice(0, 20) == 'GEO_BACKGROUND_COLOR') { //dl
-			LoadSkybox = true
-			const ortho = lineParse.trim().slice(21, lineParse.trim().indexOf('),'))
-			outputStr += `// TODO GEO_BACKGROUND_COLOR(${ortho}),\n`
-		} else if (lineParse.trim().slice(0, 20) == 'GEO_NODE_SCREEN_AREA') { //dl
-			const scrArea = lineParse.trim().slice(21, lineParse.trim().indexOf('),'))
-			outputStr += `{\n`
-			outputStr += `command: Geo.node_screen_area,\n`
-			outputStr += `args: [${scrArea}],\n`
-			outputStr += `},\n`
-		} else if (lineParse.trim().slice(0, 28) == 'GEO_CAMERA_FRUSTUM_WITH_FUNC') { //dl
-			LoadCamera = true
-			const cFrFu = lineParse.trim().slice(29, lineParse.trim().indexOf('),'))
-			outputStr += `{ command: Geo.node_perspective, args: [${cFrFu}] }\n`
-		} else if (lineParse.trim().slice(0, 14) == 'GEO_BACKGROUND') { //dl
-			LoadSkybox = true
-			const background = lineParse.trim().slice(15, lineParse.trim().indexOf('),'))
-			outputStr += `{ command: GeoLayout.node_background, args: [GeoLayout.${background}] },\n`
-		} else if (lineParse.trim().slice(0, 10) == 'GEO_CAMERA') { //dl
-			LoadCamera = true
-			const cCam = lineParse.trim().slice(11, lineParse.trim().indexOf('),'))
-			outputStr += `{\n`
-			outputStr += `command: Geo.node_camera,\n`
-			outputStr += `args: [${cCam}],\n`
-			outputStr += `},\n`
-		} else {
-			console.log("Could not parse: " + lineParse.trim())
-		}
-	}
-})
-
-outputStr = outputStr.replace(/&/g, "")
-outputStr = outputStr.replace(/ G_/g, " Gbi.G_")
-outputStr = outputStr.replace(/\(G_/g, "(Gbi.G_")
-outputStr = outputStr.replace(/\.l/g, ".l[0]")
-
-outputStr = AdditionalFiles + outputStr
-if (LoadSkybox) {outputStr = 'import { geo_skybox_main } from "../../../../game/LevelGeo"\n' + outputStr}
-if (LoadCamera) {outputStr = 'import { CameraInstance as Camera } from "../../../../game/Camera"\n' + outputStr}
-outputStr = 'import { GeoLayoutInstance as Geo } from "../../../../../engine/GeoLayout"\n' + outputStr
-//outputStr = 'import * as Gbi from "../../../../../include/gbi"\n' + outputStr
-
-var dir = mainDir + snum;
-
-MakeDirectory(dir);
-fs.writeFileSync(__dirname + '/converted/' + level + '/areas/' + areaNum + '/' + snum + '/geo.inc.js', outputStr)
+function ReadFile(input) {
+	var inputStr = fs.readFileSync(input, 'utf8')
+	var inputStr = inputStr.replace(/\r/g, "")
+	var lines = inputStr.split("\n")
+	var lines = lines.filter(line => (line.length != 0) && (line[0] != '/'))
+	return lines;
 }
-snum = snum + 1
+function CompileGeo(lines,AreaDir) {
+	var LoadCamera = false
+	var LoadSkybox = false
+	var outputStr = ""
+	var AdditionalFiles = ""
+	lines.forEach(lineParse => {
+		if (lineParse.trim().slice(0, 2) != "//") {
+			if (lineParse.trim().slice(0, 16) == 'const GeoLayout ') { //geolayout
+				const geoLayoutName = lineParse.trim().slice(16, lineParse.trim().indexOf('['))
+				outputStr += `export const ${geoLayoutName} = [\n`
+			} else if (lineParse.trim().slice(0, 2) == '};') { //geolayout end(?) (hack)
+				const geoLayoutName = lineParse.trim().slice(16, lineParse.trim().indexOf('['))
+				outputStr += `]\n`
+			} else if (lineParse.trim().slice(0, 18) == 'GEO_CULLING_RADIUS') { //cull radius
+				const cullRadius = lineParse.trim().slice(19, lineParse.trim().indexOf('),'))
+				outputStr += `{ command: Geo.node_culling_radius, args: [${cullRadius}] },\n`
+			} else if (lineParse.trim().slice(0, 13) == 'GEO_OPEN_NODE') { //open node
+				outputStr += `{ command: Geo.open_node },\n`
+			} else if (lineParse.trim().slice(0, 14) == 'GEO_CLOSE_NODE') { //close node
+				outputStr += `{ command: Geo.close_node },\n`
+			} else if (lineParse.trim().slice(0, 7) == 'GEO_END') { //end
+				outputStr += `{ command: Geo.node_end },\n`
+			} else if (lineParse.trim().slice(0, 14) == 'GEO_RENDER_OBJ') { //end
+				outputStr += `{ command: Geo.node_render_object_parent },\n`
+			} else if (lineParse.trim().slice(0, 16) == 'GEO_RENDER_RANGE') { //render range
+				const renderRange = lineParse.trim().slice(17, lineParse.trim().indexOf('),'))
+				outputStr += `{ command: Geo.node_render_range, args: [${renderRange}] },\n`
+			} else if (lineParse.trim().slice(0, 16) == 'GEO_DISPLAY_LIST') { //dl ----------- TODO : Make this search files in the same folder for the DL it's trying to render. 
+				const dList = lineParse.trim().slice(17, lineParse.trim().indexOf('),'))
+				const ArrayArgs = dList.split(', ')
+				AdditionalFiles += SearchAndApplyDLs(AreaDir,ArrayArgs[1],snum)
+				outputStr += `{ command: Geo.display_list, args: [Geo.${dList}] },\n`
+			} else if (lineParse.trim().slice(0, 7) == 'GEO_ASM') { //dl
+				const asm = lineParse.trim().slice(8, lineParse.trim().indexOf('),'))
+				outputStr += `{ command: Geo.node_generated, args: [${asm}]},\n`
+			} else if (lineParse.trim().slice(0, 11) == 'GEO_ZBUFFER') { //dl
+				const zb = lineParse.trim().slice(12, lineParse.trim().indexOf('),'))
+				outputStr += `{ command: Geo.node_master_list, args: [${zb}]},\n`
+			} else if (lineParse.trim().slice(0, 14) == 'GEO_NODE_ORTHO') { //dl
+				const ortho = lineParse.trim().slice(15, lineParse.trim().indexOf('),'))
+				outputStr += `{ command: Geo.node_ortho, args: [${ortho}]},\n`
+			} else if (lineParse.trim().slice(0, 20) == 'GEO_BACKGROUND_COLOR') { //dl
+				LoadSkybox = true
+				const ortho = lineParse.trim().slice(21, lineParse.trim().indexOf('),'))
+				outputStr += `// TODO GEO_BACKGROUND_COLOR(${ortho}),\n`
+			} else if (lineParse.trim().slice(0, 20) == 'GEO_NODE_SCREEN_AREA') { //dl
+				const scrArea = lineParse.trim().slice(21, lineParse.trim().indexOf('),'))
+				var ScrAreaFIXED = `args: [${scrArea}],\n`;
+				ScrAreaFIXED = ScrAreaFIXED.replace("SCREEN_WIDTH","canvas.width"); // Fix the screen area arguments. Cheap method.
+				ScrAreaFIXED = ScrAreaFIXED.replace("SCREEN_HEIGHT","canvas.height"); // Fix the screen area arguments. Cheap method.
+				outputStr += `{\n`
+				outputStr += `command: Geo.node_screen_area,\n`
+				outputStr += ScrAreaFIXED;
+				outputStr += `},\n`
+			} else if (lineParse.trim().slice(0, 28) == 'GEO_CAMERA_FRUSTUM_WITH_FUNC') { //dl
+				LoadCamera = true
+				const cFrFu = lineParse.trim().slice(29, lineParse.trim().indexOf('),'))
+				const ArrayArgs = cFrFu.split(', ')
+				outputStr += `{ command: Geo.node_perspective, args: [${ArrayArgs[0]}, ${ArrayArgs[1]}, ${ArrayArgs[2]}, Camera.${ArrayArgs[3]}] }\n`
+			} else if (lineParse.trim().slice(0, 14) == 'GEO_BACKGROUND') { //dl
+				LoadSkybox = true
+				const background = lineParse.trim().slice(15, lineParse.trim().indexOf('),'))
+				outputStr += `{ command: GeoLayout.node_background, args: [GeoLayout.${background}] },\n`
+			} else if (lineParse.trim().slice(0, 10) == 'GEO_CAMERA') { //dl
+				LoadCamera = true
+				const cCam = lineParse.trim().slice(11, lineParse.trim().indexOf('),'))
+				outputStr += `{\n`
+				outputStr += `command: Geo.node_camera,\n`
+				outputStr += `args: [${cCam}],\n`
+				outputStr += `},\n`
+			} else {
+				console.log("Could not parse: " + lineParse.trim())
+			}
+		}
+	})
+
+	outputStr = outputStr.replace(/&/g, "")
+	outputStr = outputStr.replace(/ G_/g, " Gbi.G_")
+	outputStr = outputStr.replace(/\(G_/g, "(Gbi.G_")
+	outputStr = outputStr.replace(/\.l/g, ".l[0]")
+
+	outputStr = AdditionalFiles + outputStr
+	if (LoadSkybox) {outputStr = 'import { geo_skybox_main } from "../../../../game/LevelGeo"\n' + outputStr}
+	if (LoadCamera) {outputStr = 'import { CameraInstance as Camera } from "../../../../game/Camera"\n' + outputStr}
+	outputStr = 'import { GeoLayoutInstance as Geo } from "../../../../../engine/GeoLayout"\n' + outputStr	
+	return outputStr;
+}
+
+var inputBase = require('os').homedir() + '/sm64ex/levels/' + level + '/areas/' +areaNum+ '/geo.inc.c' // directory of each model file
+var AreaDirBase = require('os').homedir() + '/sm64ex/levels/' + level + '/areas/' +areaNum + '/'  // used for DL checks
+	if (fs.existsSync(inputBase)) {
+		MakeDirectory(__dirname + '/converted/' + level + '/areas/' + areaNum + '/');
+		fs.writeFileSync(__dirname + '/converted/' + level + '/areas/' + areaNum + '/geo.inc.js', CompileGeo(ReadFile(inputBase),AreaDirBase))
+	}
+while (snum <= num) {
+	var input = require('os').homedir() + '/sm64ex/levels/' + level + '/areas/' +areaNum+ '/' + snum + '/geo.inc.c' 
+	var cAreaDir = require('os').homedir() + '/sm64ex/levels/' + level + '/areas/' +areaNum+ '/' + snum + '/' 
+	if (fs.existsSync(input)) {
+		MakeDirectory(__dirname + '/converted/' + level + '/areas/' + areaNum + '/' + snum + '/');
+		fs.writeFileSync(__dirname + '/converted/' + level + '/areas/' + areaNum + '/' + snum + '/geo.inc.js', CompileGeo(ReadFile(input),cAreaDir))
+	}
+	snum = snum + 1
 }

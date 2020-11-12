5e5faa97f89bc83774a0f884b912b323736a9280
Mario Reder <mreder1289@gmail.com>
2020-11-12 07:39:30 +0100

    build docker assets container

--- publish-rust-server.sh
+++ publish-rust-server.sh
@@ -15,6 +15,12 @@ docker push sm64js/sm64js-build:latest
 docker tag sm64js/sm64js-build sm64js/sm64js-build:$GIT_HASH
 docker push sm64js/sm64js-build:$GIT_HASH
 
+docker pull sm64js/sm64js-assets || true
+docker build --cache-from=sm64js/sm64js-assets -t sm64js/sm64js-assets:latest -f ./DockerfileAssets .
+docker push sm64js/sm64js-assets:latest
+docker tag sm64js/sm64js-assets sm64js/sm64js-assets:$GIT_HASH
+docker push sm64js/sm64js-assets:$GIT_HASH
+
 docker pull sm64js/sm64js || true
 docker build --cache-from=sm64js/sm64js -t sm64js/sm64js -f ./rust-server/Dockerfile .
 docker push sm64js/sm64js:latest

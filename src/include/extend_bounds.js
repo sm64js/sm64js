658e667bce3f8094634c34651a5ac9674b1e4145
snuffysasa <mattsasa@me.com>
2020-11-15 20:26:45 -0800

    camera fix, extend bounaries by only double in each dimension

--- src/include/extend_bounds.js
+++ src/include/extend_bounds.js
@@ -1,2 +1,2 @@
-export const LEVEL_BOUNDARY_EXTENDED_MAX  = 0x4000
-export const BOUNDS_EXTENSION  = 4.0
+export const LEVEL_BOUNDARY_EXTENDED_MAX  = 0x4000  //// not used
+export const BOUNDS_EXTENSION  = 2.0

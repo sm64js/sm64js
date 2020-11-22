9a0d83a92c6f2d1b3f3e64286c6eeea146a056d7
snuffysasa <mattsasa@me.com>
2020-11-22 13:26:55 -0800

    added cors check

--- serveProduction.js
+++ serveProduction.js
@@ -400,6 +400,10 @@ require('uWebSockets.js').App().ws('/*', {
     upgrade: (res, req, context) => { // a request was made to open websocket, res req have all the properties for the request, cookies etc
         // add code here to determine if ws request should be accepted or denied
         // can deny request with "return res.writeStatus('401').end()" see issue #367
+        let originHeader = req.getHeader('origin')
+        const url = new URL(originHeader)
+        const domainStr = url.hostname.substring(url.hostname.length - 11, url.hostname.length)
+        if (domainStr != ".sm64js.com" && url.hostname != "sm64js.com") return res.writeStatus('401').end()
         res.upgrade( // upgrade to websocket
            { ip: req.getHeader('x-forwarded-for') }, // 1st argument sets which properties to pass to the ws object, in this case ip address
            req.getHeader('sec-websocket-key'),

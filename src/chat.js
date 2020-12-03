b67c57dfc4bcbb8bf87a1d728e7664791c750f07
scott dikowitz <dikowitz@gmail.com>
2020-12-02 21:56:57 -0500

    sanitize messages properly and other small optimizations

--- src/chat.js
+++ src/chat.js
@@ -1,17 +1,35 @@
 ﻿import { networkData } from "./socket"
 
-const applyEmotes = (string, isMessage) => {
-    string = string.replace(/:doublek:/g, "<img height='20' width='20' src='emotes/doublek.png' alt=':doublek:' />")
-    string = string.replace(/:facepalm:/g, "<img height='20' width='20' src='emotes/facepalm.png' alt=':facepalm:' />")
-    string = string.replace(/:kappa:/g, "<img height='20' width='20' src='emotes/kappa.png' alt=':kappa:' />")
-    string = string.replace(/:mariostyle:/g, "<img height='20' width='20' src='emotes/mariostyle.gif' alt=':mariostyle:' />")
-    string = string.replace(/:pogchamp:/g, "<img height='20' width='20' src='emotes/pogchamp.png' alt=':pogchamp:' />")
-    string = string.replace(/:strange:/g, "<img height='20' width='20' src='emotes/strange.png' alt=':strange:' />")
-    string = string.replace(/:kick:/g, "<img height='20' width='20' src='emotes/kick.gif' alt=':kick:' />")
-    string = string.replace(/:shock:/g, "<img height='20' width='20' src='emotes/shock.gif' alt=':shock:' />")
-    string = string.replace(/:bup:/g, "<img height='20' width='20' src='emotes/bup.jpg' alt=':bup:' />")
-    // string.replace any other emotes in this fashion.
-    return string
+const emoteImg = {
+    doublek: 'doublek.png', facepalm: 'facepalm.png',
+    kappa: 'kappa.png', mariostyle: 'mariostyle.gif',
+    pogchamp: 'pogchamp.png', strange: 'strange.png',
+    kick: 'kick.gif', shock: 'shock.gif',
+    bup: 'bup.jpg'
+}
+
+const createEmote = (type) => {
+    const img = document.createElement('img');
+    img.height = '20';
+    img.width = '20';
+    img.alt = `:${type}:`;
+    img.src = `src/emotes/${emoteImg[type]}`;
+    return img;
+}
+
+const createMessage = (string) => {
+    const message = document.createElement('span');
+    string.split(/(:\w+:)/g).forEach(token => {
+        if (token.charAt(0) === ':' && token.charAt(token.length-1) === ':') {
+            const emoteType = token.slice(1, token.length-1);
+            if (emoteImg[emoteType]) {
+                message.appendChild(createEmote(emoteType));
+                return;
+            }
+        }
+        message.append(token);
+    });
+    return message;
 }
 
 const blockChatExtended = (str) => {
@@ -30,7 +48,7 @@ export const recvChat = (chatmsg) => {
     if (window.banPlayerList.includes(sender) || blockChatExtended(sender)) return
 
     const chatlog = document.getElementById("chatlog")
-    const node = document.createElement("LI")
+    const node = document.createElement("li")
 
     let adminTag = ""
 
@@ -39,8 +57,12 @@ export const recvChat = (chatmsg) => {
         adminTag = "(Admin)"
     }
 
-    node.innerHTML = '<strong>' + adminTag + sender + '</strong>: ' + applyEmotes(msg) + '<br/>' 
-    chatlog.appendChild(node)
+    const from = document.createElement('strong');
+    from.append(`${adminTag}${sender}`);
+    node.appendChild(from);
+    node.append(': ');
+    node.appendChild(createMessage(msg));
+    chatlog.appendChild(node);
     chatlog.scrollTop = document.getElementById("chatlog").scrollHeight
 
     if (sender == "Server") {

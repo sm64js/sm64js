0acb740d5ed9ded89f78936b700263b278425d63
snuffysasa <mattsasa@me.com>
2021-01-24 22:20:36 -0600

    finished merging in bobombs into MMO, but commenting them out because they are not synced yet

--- src/levels/bob/areas/1/macro.inc.js
+++ src/levels/bob/areas/1/macro.inc.js
@@ -2,7 +2,7 @@ import { MACRO_GOOMBA, MACRO_GOOMBA_TRIPLET_SPAWNER, MACRO_BOBOMB } from "../../
 
 export const bob_seg7_macro_objs = [
 
-    { preset: MACRO_GOOMBA, yaw: 0, pos: [-2713, 152, 5778], param: 0 },
+/*    { preset: MACRO_GOOMBA, yaw: 0, pos: [-2713, 152, 5778], param: 0 },
     { preset: MACRO_GOOMBA, yaw: 0, pos: [-342, 400, 5433], param: 0 },
     { preset: MACRO_GOOMBA_TRIPLET_SPAWNER, yaw: 0, pos: [3640, 768, 6280], param: 0 },
     { preset: MACRO_GOOMBA_TRIPLET_SPAWNER, yaw: 0, pos: [6060, 877, 2000], param: 0 },
@@ -18,6 +18,6 @@ export const bob_seg7_macro_objs = [
     { preset: MACRO_BOBOMB, yaw: 0, pos: [-1750, 0, -2800], param: 0 },
     { preset: MACRO_BOBOMB, yaw: 0, pos: [-1400, 0, -950], param: 0 },
     { preset: MACRO_BOBOMB, yaw: 0, pos: [-2650, 0, 1750], param: 0 },
-    { preset: MACRO_BOBOMB, yaw: 0, pos: [-1900, 0, 3450], param: 0 },
+    { preset: MACRO_BOBOMB, yaw: 0, pos: [-1900, 0, 3450], param: 0 },*/
 
 ]

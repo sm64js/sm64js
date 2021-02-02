d34da421376ed2ba411ea79ca1ac8c524e3e9948
luphoria <luphoria@innocent.com>
2021-02-01 18:51:05 -0600

    uncomment ttm

--- src/levels/level_defines.js
+++ src/levels/level_defines.js
@@ -1,5 +1,5 @@
 import { LevelCommandsInstance as LevelCommands } from "../engine/LevelCommands"
-import { LEVEL_CASTLE_GROUNDS, LEVEL_BOB, LEVEL_CCM, LEVEL_WF, LEVEL_PSS, /*LEVEL_TTM, */LEVEL_HMC } from "./level_defines_constants"
+import { LEVEL_CASTLE_GROUNDS, LEVEL_BOB, LEVEL_CCM, LEVEL_WF, LEVEL_PSS, LEVEL_TTM, LEVEL_HMC } from "./level_defines_constants"
 import { level_castle_grounds_entry } from "./castle_grounds/script"
 import { level_bob_entry } from "./bob/script"
 import { level_ccm_entry } from "./ccm/script"
@@ -25,11 +25,10 @@ export const level_defines_list = [
         command: LevelCommands.jump_if,
         args: [LevelCommands.OP_EQ, LEVEL_PSS, level_pss_entry]
     },
-    /*
     {
         command: LevelCommands.jump_if,
         args: [LevelCommands.OP_EQ, LEVEL_TTM, level_ttm_entry]
-    },*/
+    },
     {
         command: LevelCommands.jump_if,
         args: [LevelCommands.OP_EQ, LEVEL_WF, level_wf_entry]

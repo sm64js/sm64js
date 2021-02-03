39a823492fdff431a3a3e8aa1556ff99aca7f79d
luphoria <luphoria@innocent.com>
2021-02-03 14:58:22 -0600

    add CASTLE_COURTYARD

--- src/levels/level_defines.js
+++ src/levels/level_defines.js
@@ -1,5 +1,5 @@
 import { LevelCommandsInstance as LevelCommands } from "../engine/LevelCommands"
-import { LEVEL_CASTLE_GROUNDS, LEVEL_BOB, LEVEL_CCM, LEVEL_WF, LEVEL_PSS, LEVEL_TTM, LEVEL_HMC, LEVEL_BBH } from "./level_defines_constants"
+import { LEVEL_CASTLE_GROUNDS, LEVEL_BOB, LEVEL_CCM, LEVEL_WF, LEVEL_PSS, LEVEL_TTM, LEVEL_HMC, LEVEL_BBH, LEVEL_CASTLE_COURTYARD } from "./level_defines_constants"
 import { level_castle_grounds_entry } from "./castle_grounds/script"
 import { level_bob_entry } from "./bob/script"
 import { level_ccm_entry } from "./ccm/script"
@@ -8,11 +8,16 @@ import { level_ttm_entry } from "./ttm/script"
 import { level_wf_entry } from "./wf/script"
 import { level_hmc_entry } from "./hmc/script"
 import { level_bbh_entry } from "./bbh/script"
+import { level_castle_courtyard_entry } from "./castle_courtyard/script"
 
 export const level_defines_list = [
     {
         command: LevelCommands.jump_if,
         args: [LevelCommands.OP_EQ, LEVEL_CASTLE_GROUNDS, level_castle_grounds_entry]
+    },
+    {
+        command: LevelCommands.jump_if,
+        args: [LevelCommands.OP_EQ, LEVEL_CASTLE_COURTYARD, level_castle_courtyard_entry]
     },
     {
         command: LevelCommands.jump_if,
--- src/levels/level_defines_constants.js
+++ src/levels/level_defines_constants.js
@@ -1,4 +1,5 @@
 export const LEVEL_CASTLE_GROUNDS = 16
+export const LEVEL_CASTLE_COURTYARD = 621
 export const LEVEL_BOB = 9
 export const LEVEL_CCM = 5
 export const LEVEL_PSS = 27

d2243f3c1a7f0815ab11bfd396b9d55c234a189b
J4ckr07 <66310147+J4ckr07@users.noreply.github.com>
2021-02-06 17:07:38 +0000

    Update Skybox.js for ssl skybox

--- src/game/Skybox.js
+++ src/game/Skybox.js
@@ -2,6 +2,7 @@ import { atan2s, guOrtho } from "../engine/math_util"
 import  * as Gbi from "../include/gbi"
 import { dl_skybox_begin, dl_skybox_tile_tex_settings, dl_skybox_end, dl_draw_quad_verts_0123 } from "../common_gfx/segment2"
 import { water_skybox_ptrlist } from "../textures/skyboxes/water_skybox"
+import { ssl_skybox_ptrlist } from "../textures/skyboxes/ssl_skybox"
 import { make_vertex } from "./GeoMisc"
 
 const canvas = document.querySelector('#gameCanvas')
@@ -26,7 +27,7 @@ class SkyBox {
             null, //wdw_skybox_ptrlist,
             null, //cloud_floor_skybox_ptrlist,
             null, //ccm_skybox_ptrlist,
-            null, //ssl_skybox_ptrlist,
+            ssl_skybox_ptrlist,
             null, //bbh_skybox_ptrlist,
             null, //bidw_skybox_ptrlist,
             null, //clouds_skybox_ptrlist,

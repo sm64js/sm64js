fd2f9663694574c9602e7643e2bb70b06992ffec
luphoria <luphoria@innocent.com>
2021-02-06 16:02:51 -0600

    fix level defines from porn reference

--- src/levels/level_defines_constants.js
+++ src/levels/level_defines_constants.js
@@ -1,6 +1,6 @@
 export const LEVEL_CASTLE_GROUNDS = 16
-export const LEVEL_CASTLE_COURTYARD = 621
-export const LEVEL_CASTLE_INSIDE = 34
+export const LEVEL_CASTLE_COURTYARD = 26
+export const LEVEL_CASTLE_INSIDE = 6
 export const LEVEL_BOB = 9
 export const LEVEL_CCM = 5
 export const LEVEL_PSS = 27

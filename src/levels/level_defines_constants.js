ffe1b16917635a6e95219a396a48cc07d1c704e7
snuffysasa <mattsasa@me.com>
2021-02-13 06:12:33 -0600

    change castle inside level number to match MMO

--- src/levels/level_defines_constants.js
+++ src/levels/level_defines_constants.js
@@ -1,7 +1,7 @@
 export const LEVEL_CASTLE_GROUNDS = 16
 export const LEVEL_CASTLE_COURTYARD = 26
 export const LEVEL_CASTLE = 6
-export const LEVEL_CASTLE_2 = 6.2
+export const LEVEL_CASTLE_2 = 602
 export const LEVEL_BOB = 9
 export const LEVEL_CCM = 5
 export const LEVEL_PSS = 27

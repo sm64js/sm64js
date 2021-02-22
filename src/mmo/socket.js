efdea7400cb8666ff41adf5fdfe2ba5447533797
snuffysasa <mattsasa@me.com>
2021-02-22 01:16:24 -0600

    in prod point to test server

--- src/mmo/socket.js
+++ src/mmo/socket.js
@@ -50,7 +50,7 @@ if (gameID) { document.getElementById("mapSelect").hidden = true }
 const websocketServerPath = process.env.NODE_ENV === 'rust'
     ? `${url.protocol == "https:" ? "wss" : "ws"}://${window.location.host}/ws/` // Tarnadas - Rust Server
     : url.protocol == "https:" // Snuffy - sm64js.com
-        ? `wss://server.sm64js.com/websocket/` // production
+        ? `wss://mmo-server-test.sm64js.com/websocket/` // production
         : `ws://${url.hostname}:3000` // local testing
 
 const socket = new WebSocket(websocketServerPath)

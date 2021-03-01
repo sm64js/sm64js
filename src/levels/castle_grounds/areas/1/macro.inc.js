a176eb1897a0992d9679ad353a554be776bb6b6f
snuffysasa <mattsasa@me.com>
2021-02-28 19:27:37 -0600

    goombas on castle grounds start

---
+++ src/levels/castle_grounds/areas/1/macro.inc.js
@@ -0,0 +1,12 @@
+import { MACRO_GOOMBA, MACRO_GOOMBA_TRIPLET_SPAWNER, MACRO_BOBOMB, MACRO_CHAIN_CHOMP, MACRO_COIN_LINE_HORIZONTAL, MACRO_COIN_RING_HORIZONTAL, MACRO_RED_COIN, MACRO_COIN_RING_VERTICAL, MACRO_YELLOW_COIN } from "../../../../include/macro_presets"
+
+export const castle_seg7_macro_objs = [
+    { preset: MACRO_GOOMBA, yaw: 0, pos: [-1128, 260, 4664], param: 0 },
+    { preset: MACRO_GOOMBA, yaw: 0, pos: [-1528, 260, 4664], param: 0 },
+    { preset: MACRO_GOOMBA, yaw: 0, pos: [-1328, 260, 4864], param: 0 },
+    { preset: MACRO_GOOMBA, yaw: 0, pos: [-1328, 260, 4464], param: 0 },
+    { preset: MACRO_GOOMBA, yaw: 0, pos: [-1128, 260, 4464], param: 0 },
+    { preset: MACRO_GOOMBA, yaw: 0, pos: [-1528, 260, 4864], param: 0 },
+    { preset: MACRO_GOOMBA, yaw: 0, pos: [-1128, 260, 4864], param: 0 },
+    { preset: MACRO_GOOMBA, yaw: 0, pos: [-1528, 260, 4464], param: 0 },
+]
--- src/levels/castle_grounds/script.js
+++ src/levels/castle_grounds/script.js
@@ -7,6 +7,8 @@ import { castle_grounds_seg7_collision_level } from "./areas/1/collision.inc"
 import { LevelUpdateInstance as LevelUpdate } from "../../game/LevelUpdate"
 import { bubbly_tree_geo } from "../../actors/tree/geo.inc"
 import { MODEL_BOB_BUBBLY_TREE, MODEL_LEVEL_GEOMETRY_03, MODEL_CASTLE_GROUNDS_FLAG } from "../../include/model_ids"
+import { castle_seg7_macro_objs } from "./areas/1/macro.inc"
+import { script_func_global_1 } from "../global_scripts"
 
 
 export const script_func_local_3 = [
@@ -21,12 +23,14 @@ export const script_func_local_3 = [
 export const level_castle_grounds_entry = [
     { command: LevelCommands.init_level },
     { command: LevelCommands.init_mario, args: [1, 1, bhvMario] },
+    { command: LevelCommands.jump_link, args: [script_func_global_1] }, 
     { command: LevelCommands.load_model_from_geo, args: [MODEL_LEVEL_GEOMETRY_03, castle_grounds_geo_0006F4] },
     { command: LevelCommands.load_model_from_geo, args: [MODEL_BOB_BUBBLY_TREE, bubbly_tree_geo] },
     { command: LevelCommands.load_model_from_geo, args: [MODEL_CASTLE_GROUNDS_FLAG, castle_grounds_geo_000660] },
     { command: LevelCommands.begin_area, args: [1, castle_grounds_geo_00073C] },
     { command: LevelCommands.jump_link, args: [script_func_local_3] },
     { command: LevelCommands.terrain, args: [castle_grounds_seg7_collision_level] },
+    { command: LevelCommands.macro_objects, args: [castle_seg7_macro_objs] },
     { command: LevelCommands.end_area },
     { command: LevelCommands.free_level_pool },
     { command: LevelCommands.set_mario_pos, args: [1, 180, -1328, 260, 4664] },

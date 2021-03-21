ff91e6d9697714ea18dc6c4bd1315c13faa1b1d8
SMGee4 <77451531+SMGee4@users.noreply.github.com>
2021-03-21 19:03:43 -0400

    Delete wrong doora directory

--- door/anims/anim_03015208.inc.js
+++
@@ -1,6 +0,0 @@
-export const door_seg3_anim_03015208 = {
-    flags: 0, unk02: 0, unk04: 0, unk06: 0, unk08: 0x50,
-    unk0A: (door_seg3_animindex_030151CC.length / 6) - 1,
-    values: door_seg3_animvalue_030151C8,
-    indices: door_seg3_animindex_030151CC,
-}

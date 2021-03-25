af93f425c902758f7d7228d29174e3044c0da24d
Joe Holt <1596163+joe07734@users.noreply.github.com>
2021-03-24 23:45:19 -1000

    less wordy level scripts
    Support an alternate format for level scripts that are less wordy.
    
    { command: LevelCommandsInstance.sleep, args: [2] }
    
    becomes
    
     ['sleep', 2]
    
    Both styles are valid and can be mixed. It's easier to work in the concise style for manually created scripts.
    
    Convert a few level scripts as an example.
    
    Also rework LevelCommands methods to be clearer with args.

--- src/levels/main_entry/entry.js
+++ src/levels/main_entry/entry.js
@@ -1,10 +1,9 @@
-import { LevelCommandsInstance } from "../../engine/LevelCommands"
 import { level_intro_entry_1 } from "../intro/script"
 
 export const level_script_entry = [
-    { command: LevelCommandsInstance.init_level },
-    { command: LevelCommandsInstance.sleep, args: [2] },
-    { command: LevelCommandsInstance.blackout, args: [false] },
-    { command: LevelCommandsInstance.set_register, args: [0] },
-    { command: LevelCommandsInstance.execute, args: [level_intro_entry_1] } 
+    ['init_level'],
+    ['sleep', 2],
+    ['blackout', false],
+    ['set_register', 0],
+    ['execute', level_intro_entry_1]
 ]

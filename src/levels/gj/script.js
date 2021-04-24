8e5e92f6e48ce244e16b909f1f5bc9c4c60def21
Agent 11 <techgod18@outlook.com>
2021-04-24 11:33:51 -0400

    Add Glider Jungle Clientside
    Well, here it is, my first custom level! I plan on making more next, it was a fun experience.

---
+++ src/levels/gj/script.js
@@ -0,0 +1,16 @@
+import { LevelCommandsInstance as LevelCommands } from "../../engine/LevelCommands"
+import { bhvMario } from "../../game/BehaviorData"
+import { LevelUpdateInstance as LevelUpdate } from "../../game/LevelUpdate"
+import { gj_area_1_geo } from "./areas/1/geo.inc"
+import { gj_area_1_collision } from "./areas/1/collision.inc"
+
+export const level_gj_entry = [
+    { command: LevelCommands.init_level },
+    { command: LevelCommands.init_mario, args: [1, 1, bhvMario] },
+    { command: LevelCommands.begin_area, args: [1, gj_area_1_geo] },
+    { command: LevelCommands.terrain, args: [gj_area_1_collision] },
+    { command: LevelCommands.end_area },
+	{ command: LevelCommands.set_mario_pos, args: [1, -90, 5992, 381, -20] },
+    { command: LevelCommands.call, args: [0, LevelUpdate.lvl_init_or_update, LevelUpdate] },
+    { command: LevelCommands.call_loop, args: [1, LevelUpdate.lvl_init_or_update, LevelUpdate] },
+]

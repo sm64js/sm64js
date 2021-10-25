6e230b33c2b14ce099c4ceb1991ae4f7e83fb687
0x2480 <44932965+TERRAERROR@users.noreply.github.com>
2021-10-24 21:18:48 -0700

    Fix
    Forgot to just copy the position; this caused collision issues.

--- src/game/Mario.js
+++ src/game/Mario.js
@@ -1447,7 +1447,7 @@ const update_mario_inputs = (m) => {
 
 const update_mario_info_for_cam = (m) => {
     m.marioBodyState.action = m.action
-    m.marioBodyState.torsoPos = m.pos
+    m.marioBodyState.torsoPos = [...m.pos]
     m.marioBodyState.torsoPos[1] += 22 // about torso height
     m.statusForCamera.action = m.action
 

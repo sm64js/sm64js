9c2c5e3c5af5bcbf9dec0f45ce4d387402bc812e
Agent-11 / Agent X <44549182+Agent-11@users.noreply.github.com>
2021-11-06 23:14:21 -0400

    Change culling range

--- src/actors/tree/geo.inc.js
+++ src/actors/tree/geo.inc.js
@@ -2,7 +2,7 @@ import { GeoLayoutInstance as Geo } from "../../engine/GeoLayout"
 import { tree_seg3_dl_0302FEE8, tree_seg3_dl_03032088, tropic_tree_dl } from "./model.inc"
 
 export const bubbly_tree_geo = [
-    { command: Geo.node_culling_radius, args: [800] },
+    { command: Geo.node_culling_radius, args: [2000] },
     { command: Geo.open_node },
     { command: Geo.display_list, args: [Geo.LAYER_ALPHA, tree_seg3_dl_0302FEE8] },
     { command: Geo.close_node },
@@ -10,7 +10,7 @@ export const bubbly_tree_geo = [
 ]
 
 export const snow_tree_geo = [
-    { command: Geo.node_culling_radius, args: [800] },
+    { command: Geo.node_culling_radius, args: [2000] },
     { command: Geo.open_node },
     { command: Geo.display_list, args: [Geo.LAYER_ALPHA, tree_seg3_dl_03032088] },
     { command: Geo.close_node },
@@ -18,7 +18,7 @@ export const snow_tree_geo = [
 ]
 
 export const tropic_tree_geo = [
-    { command: Geo.node_culling_radius, args: [800] },
+    { command: Geo.node_culling_radius, args: [2000] },
     { command: Geo.open_node },
     { command: Geo.display_list, args: [Geo.LAYER_ALPHA, tropic_tree_dl] },
     { command: Geo.close_node },
--- src/levels/castle_grounds/areas/1/3/geo.inc.js
+++ src/levels/castle_grounds/areas/1/3/geo.inc.js
@@ -2,7 +2,7 @@ import { GeoLayoutInstance as Geo } from "../../../../../engine/GeoLayout"
 import { castle_grounds_seg7_dl_0700A290 } from "./model.inc"
 
 export const castle_grounds_geo_0006F4 = [
-    { command: Geo.node_culling_radius, args: [2100] },
+    { command: Geo.node_culling_radius, args: [3000] },
     { command: Geo.open_node },
     { command: Geo.display_list, args: [Geo.LAYER_OPAQUE, castle_grounds_seg7_dl_0700A290] },
     { command: Geo.close_node },

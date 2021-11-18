1cef70e5da53cafc928ced4708dbabd0d4c45644
Agent-11 / Agent X <44549182+Agent-11@users.noreply.github.com>
2021-11-17 22:29:12 -0500

    Mushroom Raceway update

--- src/levels/raceway/areas/1/geo.inc.js
+++ src/levels/raceway/areas/1/geo.inc.js
@@ -3,7 +3,8 @@ import { CameraInstance as Camera } from "../../../../game/Camera"
 import { geo_skybox_main } from "../../../../game/LevelGeo"
 import { geo_movtex_draw_water_regions } from "../../../../game/MovingTexture"
 
-import { raceway_DL } from "./1/model.inc"
+import { raceway_dl_bordering_mesh_layer_1, raceway_dl_map_mesh_layer_1,
+raceway_dl_map_mesh_layer_4 } from "./1/model.inc"
 
 const canvas = document.querySelector('#gameCanvas')
 
@@ -23,7 +24,9 @@ export const raceway_area_1_geo = [
 	{ command: Geo.open_node },
 	{ command: Geo.node_camera, args: [1, 0, 2000, 6000, 0, -2200, 0, Camera.geo_camera_main]},
 	{ command: Geo.open_node },
-	{ command: Geo.display_list, args: [Geo.LAYER_OPAQUE, raceway_DL] },
+	{ command: Geo.display_list, args: [Geo.LAYER_OPAQUE, raceway_dl_bordering_mesh_layer_1] },
+	{ command: Geo.display_list, args: [Geo.LAYER_OPAQUE, raceway_dl_map_mesh_layer_1] },
+	{ command: Geo.display_list, args: [Geo.LAYER_ALPHA, raceway_dl_map_mesh_layer_4] },
 	{ command: Geo.node_generated, args: [0x3901, geo_movtex_draw_water_regions]},
 	{ command: Geo.node_render_object_parent },
 	{ command: Geo.close_node },

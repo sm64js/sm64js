f1e2660cc84faf2883528885276cad3b139a4867
Agent-11 / Agent X <44549182+Agent-11@users.noreply.github.com>
2021-12-25 19:59:13 -0500

    Snowy Glider Jungle and website fixes (#614)
    * Lots of site rewrite fixes
    
    Updated a lot of lines to make the site more responsive, put the chat aside the game screen, and lots more other improvements.
    
    * Customizer menu hotfix
    
    Changed the background image to the correct one. Nice job Eros you already messed something up :trollface:
    
    * Some important problems solved, buttons changed
    
    Some stuff has been fixed since the site didn't work properly yet, still not perfect.
    New problems have been commented on by me so make sure to check those out if possible.
    
    * Game canvas and chat size hotfixes
    
    * Hotfix #2
    
    * Hotfix #3
    
    * Make the chat not disappear when in widescreen mode
    
    * Disable the respawn button's update code
    
    * Fix seesaw flag bug
    
    The flag's model is replaced with the seesaw likely because of model
    loading in the script being different due to the new level commands.
    
    * Last fixes for a while
    
    As requested, the boxes over the gameCanvas are all the same size now.
    The controls panel has been reorganized.
    And the chat has been fixed in widescreen mode.
    
    * Revert respawn button to old css method
    
    This will make it look a bit worse but it'll work again, I didn't want to leave it not functioning as intended.
    
    * Update Mario and Luigi cap textures
    
    * Remove leveling system from display name
    
    * Fix site bugs
    
    * Minor size adjustment
    
    * Save widescreen setting
    
    Untested but should work in theory.
    
    * Remove useless coment
    
    * Snow in GJ
    
    * Stage index.js for snow
    
    * Finish up rest of snow system
    
    * Snowy Glider Jungle
    
    Also decided to just render snow on textCanvas.
    
    * Fix viewport bug
    
    * HTML fixes
    
    * Update caniuse-lite
    
    * Revert caniuse-lite update, code breaker
    
    * Fix rules z-index
    
    Chatlog was overlaying it.
    
    * Remove need of fxCanvas
    
    * Fix music div placement
    
    Co-authored-by: eros71-dev <16540103+eros71-dev@users.noreply.github.com>
    Co-authored-by: Agent-11 / Agent X <Agent-11@users.noreply.github.com>

--- src/levels/gj/areas/1/geo.inc.js
+++ src/levels/gj/areas/1/geo.inc.js
@@ -3,7 +3,10 @@ import { CameraInstance as Camera } from "../../../../game/Camera"
 import { geo_skybox_main } from "../../../../game/LevelGeo"
 import { geo_movtex_draw_water_regions } from "../../../../game/MovingTexture"
 
-import { gj_DL, gj2_DL } from "./1/model.inc"
+import { gj_dl_fountain_mesh_layer_1, gj_dl_hut_mesh_layer_1, gj_dl_jungle_mesh_layer_1,
+	gj_dl_ice_mesh_layer_1, gj_dl_trees_mesh_layer_1, gj_dl_launch2_mesh, gj_dl_launch3_mesh,
+	gj_dl_launch4_mesh, gj_dl_launch5_mesh
+} from "./1/model.inc"
 
 const canvas = document.querySelector('#gameCanvas')
 
@@ -23,9 +26,16 @@ export const gj_area_1_geo = [
 	{ command: Geo.open_node },
 	{ command: Geo.node_camera, args: [1, 0, 2000, 6000, 0, -2200, 0, Camera.geo_camera_main]},
 	{ command: Geo.open_node },
-	{ command: Geo.display_list, args: [Geo.LAYER_OPAQUE, gj_DL] },
-	{ command: Geo.display_list, args: [Geo.LAYER_ALPHA, gj2_DL] },
-	{ command: Geo.node_generated, args: [0x3701, geo_movtex_draw_water_regions]},
+	{ command: Geo.display_list, args: [Geo.LAYER_OPAQUE, gj_dl_fountain_mesh_layer_1] },
+	{ command: Geo.display_list, args: [Geo.LAYER_OPAQUE, gj_dl_hut_mesh_layer_1] },
+	{ command: Geo.display_list, args: [Geo.LAYER_OPAQUE, gj_dl_jungle_mesh_layer_1] },
+	{ command: Geo.display_list, args: [Geo.LAYER_OPAQUE, gj_dl_ice_mesh_layer_1] },
+	{ command: Geo.display_list, args: [Geo.LAYER_OPAQUE, gj_dl_trees_mesh_layer_1] },
+	{ command: Geo.display_list, args: [Geo.LAYER_OPAQUE, gj_dl_launch2_mesh] },
+	{ command: Geo.display_list, args: [Geo.LAYER_OPAQUE, gj_dl_launch3_mesh] },
+	{ command: Geo.display_list, args: [Geo.LAYER_OPAQUE, gj_dl_launch4_mesh] },
+	{ command: Geo.display_list, args: [Geo.LAYER_OPAQUE, gj_dl_launch5_mesh] },
+	// { command: Geo.node_generated, args: [0x3701, geo_movtex_draw_water_regions]},
 	{ command: Geo.node_render_object_parent },
 	{ command: Geo.close_node },
 	{ command: Geo.close_node },

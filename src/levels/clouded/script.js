d71fbb5af51e2abb77b5aa53b5485137dd46f6b4
Agent-11 <44549182+Agent-11@users.noreply.github.com>
2022-03-10 19:25:58 -0500

    Fix missing song and add new song

--- src/levels/clouded/script.js
+++ src/levels/clouded/script.js
@@ -1,4 +1,5 @@
-import { LevelCommandsInstance as LevelCommands } from "../../engine/LevelCommands"
+import { LevelCommandsInstance as LevelCommands, SET_BACKGROUND_MUSIC } from "../../engine/LevelCommands"
+import { SEQ_LEVEL_WATER } from "../../include/seq_ids"
 import { bhvMario, bhvCastleFlagWaving } from "../../game/BehaviorData"
 import { LevelUpdateInstance as LevelUpdate } from "../../game/LevelUpdate"
 import { clouded_area_1_geo } from "./areas/1/geo.inc"
@@ -11,6 +12,7 @@ export const level_clouded_entry = [
     { command: LevelCommands.begin_area, args: [1, clouded_area_1_geo] },
     { command: LevelCommands.place_object, args: [/*model*/ MODEL_STAR, /*pos*/ 0, 0, 0, /*angle*/ 0, 0, 0, /*behParam*/ 0x00000000, /*beh*/ bhvCastleFlagWaving] },
     { command: LevelCommands.terrain, args: [clouded_area_1_collision] },
+    SET_BACKGROUND_MUSIC(/*settingsPreset*/ 0x0000, /*seq*/ SEQ_LEVEL_WATER),
     { command: LevelCommands.end_area },
 	{ command: LevelCommands.set_mario_pos, args: [1, 0, -67, -247, 5186] },
     { command: LevelCommands.call, args: [0, LevelUpdate.lvl_init_or_update, LevelUpdate] },
